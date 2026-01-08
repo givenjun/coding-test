c011c4129bbe4d9ebd450c0241e742f6497650db
givenjun <152844483+givenjun@users.noreply.github.com>
2026-01-08 20:56:37 +0900

    [Silver II] Title: 스택 수열, Time: 312 ms, Memory: 27296 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,58 +1,47 @@
 import java.io.*;
-import java.util.*;
+import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+        public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
 
         int N = Integer.parseInt(br.readLine());
-        StringBuilder str = new StringBuilder();
-
+        int[] ans = new int[N];
         Stack<Integer> stack = new Stack<>();
-        boolean[] visited = new boolean[N + 1];
-        int memory = 0;
 
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            
-            if (i == 0) {
-                for (int j = 0; j < num; j++) {
-                    stack.push(j + 1);
-                    str.append("+\n");
+            ans[i] = Integer.parseInt(br.readLine());    
+        }
+
+        int num = 1;
+        boolean res = true;
+        for (int i = 0; i < N; i++) {
+            if (ans[i] >= num) {
+                while (ans[i] >= num) {
+                    stack.push(num++);
+                    sb.append("+\n");
                 }
-                memory = stack.pop();
-                visited[memory] = true;
-                str.append("-\n");
+                stack.pop();
+                sb.append("-\n");
             }
             else {
-                if (memory < num) {
-                    for (int j = memory; j < num; j++) {
-                        if (visited[j + 1]) continue;
-                        stack.push(j + 1);
-                        str.append("+\n");
-                    }
-                    memory = stack.pop();
-                    visited[memory] = true;
-                    str.append("-\n");
+                int a = stack.pop();
+                if (a > ans[i]) {
+                    System.out.print("NO\n");
+                    res = false;
+                    break;
                 }
-                else if (memory > num) {
-                    if (!stack.isEmpty() && stack.peek() == num) {
-                        memory = stack.pop();
-                        visited[memory] = true;
-                        str.append("-\n");
-                    } else {
-                        str.append("NO\n");
-                    }
-                }
+                else sb.append("-\n");
             }
         }
-        String s = str.toString();
-        if (!s.contains("NO")) bw.write(s);
-        else bw.write("NO");
-
-        bw.flush(); 
-        bw.close();
+        if (res) {
+            bw.write(sb.toString());
+            bw.flush();
+            bw.close();
+        }
     }
 }
