2a26ed930de11c29ce3fd6a2ceb6afd5d203eaff
givenjun <152844483+givenjun@users.noreply.github.com>
2026-01-10 20:10:09 +0900

    [Gold IV] Title: 오큰수, Time: 884 ms, Memory: 168068 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        int[] ans = new int[N];
+        String[] S = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(S[i]);
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            while(!stack.isEmpty() && (A[i] > A[stack.peek()])) {
+                int idx = stack.pop();  
+                ans[idx] = A[i];
+            }
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()) {
+            int idx = stack.pop();
+            ans[idx] = -1;
+        } 
+
+        for (int a : ans) {
+            bw.write(a + " ");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
